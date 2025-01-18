95868c2954526977491b3d780c2d15f0cdfadc51
Muhammad Zharfan <101034407+muhzarfan@users.noreply.github.com>
2025-01-18 23:31:40 +0700

    rapihin handler.js

--- src/server/handler.js
+++ src/server/handler.js
@@ -12,7 +12,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 const storage = multer.memoryStorage();
 const upload = multer({
     storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, 
+    limits: { fileSize: 5 * 1024 * 1024 },
     fileFilter: (req, file, cb) => {
         if (!file.mimetype.startsWith('image/')) {
             return cb(new Error('Only image files are allowed!'), false);
