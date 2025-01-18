b78bd246969261d346802852184ee401070f0048
Muhammad Zharfan <101034407+muhzarfan@users.noreply.github.com>
2025-01-18 15:47:24 +0700

    chatbot fix

--- src/server/ChatbotHandler.js
+++ src/server/ChatbotHandler.js
@@ -105,7 +105,7 @@ class ChatbotHandler {
             "answer": "We believe that success comes from strong teamwork, where every division in our team is committed to growth and collaboration, both internally and with our clients as partners. By deeply understanding our clients’ needs, products, target market, and goals, we deliver practical, target-driven strategies to exceed expectations. With professionalism, energy, and efficiency, we craft impactful communication solutions that balance cost-effectiveness and results. Guided by meticulous planning and agility, we remain flexible and adaptive to overcome challenges and seize opportunities, ensuring the best outcomes for our clients."
         },
         {
-            "question": "What benefit does D'SNAP! provide in events?",
+            "question": "Benefit of D'SNAP! company ?",
             "answer": "Booth, Event Planning & Management, Set Decoration & Design, Technology & AV Support, Talent & Entertainment, Event Promotion & Marketing, Catering & Food Services, Registration & Tickets, and Event Photography & Videography."
         },
         {
