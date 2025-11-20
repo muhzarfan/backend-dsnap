2df8ac14a6b6d93307845d50c8b109a822a628f5
Muhammad Zharfan <101034407+muhzarfan@users.noreply.github.com>
2025-11-20 11:35:30 +0700

    Add authentication

--- src/server/routes.js
+++ src/server/routes.js
@@ -8,16 +8,16 @@ const chatbotHandler = new ChatbotHandler();
 // Routes portfolio CRUD
 router.get('/portfolio', handler.getPortfolios);
 router.get('/portfolio/:id', handler.getPortfolioById);
-router.post('/portfolio', handler.createPortfolio);
-router.put('/portfolio/:id', handler.updatePortfolio);
-router.delete('/portfolio/:id', handler.deletePortfolio);
+router.post('/portfolio', handlers.authMiddleware, handler.createPortfolio);
+router.put('/portfolio/:id', handlers.authMiddleware, handler.updatePortfolio);
+router.delete('/portfolio/:id', handlers.authMiddleware, handler.deletePortfolio);
 
 // Routes order CRUD
 router.get('/order', handler.getOrder);
 router.get('/order/:id', handler.getOrdersById);
-router.post('/order', handler.createOrder);
-router.put('/order/:id', handler.updateOrder);
-router.delete('/order/:id', handler.deleteOrder);
+router.post('/order', handlers.authMiddleware, handler.createOrder);
+router.put('/order/:id', handlers.authMiddleware, handler.updateOrder);
+router.delete('/order/:id', handlers.authMiddleware, handler.deleteOrder);
 
 // Route login
 router.post('/login', handler.loginHandler);
