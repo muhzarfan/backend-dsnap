61080eb079e5d3dd8eea9a9453e62e70eab8af41
Muhammad Zharfan <101034407+muhzarfan@users.noreply.github.com>
2025-11-20 11:30:48 +0700

    Enhance authMiddleware
    Added check for JWT_SECRET in environment variables and updated error messages.

--- src/server/authMiddleware.js
+++ src/server/authMiddleware.js
@@ -1,16 +1,19 @@
 const jwt = require('jsonwebtoken');
-const secretKey = process.env.JWT_SECRET;
-
+const secretKey = process.env.JWT_SECRET; 
 
 const authMiddleware = (req, res, next) => {
+    if (!secretKey) {
+        console.error("JWT_SECRET is not defined in environment variables.");
+        return res.status(500).json({ error: "Server configuration error." });
+    }
+    
     const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
-        return res.status(401).json({ error: "Unauthorized" });
+        return res.status(401).json({ error: "Unauthorized: Token tidak tersedia" });
     }
 
     try {
-        const secretKey = "your_secret_key";
-        const decoded = jwt.verify(token, secretKey);
+        const decoded = jwt.verify(token, secretKey); 
         req.user = decoded;
         next();
     } catch (error) {
